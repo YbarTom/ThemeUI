5245fe2221b090c7c4a4855570b9bb94692a945e
YbarTom <a22tomybanog@inspedralbes.cat>
2024-01-24 14:28:40 +0100

    theme ui project

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ThemeUI-test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
