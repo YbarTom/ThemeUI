ff13fb3f9d6510b684d9827217fb7d120b21bf25
MikiDix <a22miqdervil@inspedralbes.cat>
2024-01-24 14:55:30 +0100

    ns

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,31 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `ThemeUI-test`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-theme-ui"]
-};
+  plugins: [
+    { resolve: 'gatsby-plugin-theme-ui' },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Nombre del Sitio",
+        short_name: "Nombre Corto",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "standalone",
+        icon: "src/images/icon.png", // Ruta a tu icono (debe estar dentro de la carpeta "src")
+        sizes: "196x196",
+        icon_options: {
+          purpose: `maskable , any`,
+
+        },
+
+      },
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+
+    },
+  ],
+}
