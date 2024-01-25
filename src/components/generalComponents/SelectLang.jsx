73a1db852021b6c9eb9fbed605201251d0bdbd15
YbarTom <a22tomybanog@inspedralbes.cat>
2024-01-25 15:44:29 +0100

    desplegable idioma

--- src/components/generalComponents/SelectLang.jsx
+++ src/components/generalComponents/SelectLang.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { Select, Box } from 'theme-ui'
+import React, { useState } from 'react';
+import { Select } from 'theme-ui';
+import { useTranslation } from 'react-i18next';
 
 const SelectLang = () => {
-    return (
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState('es'); // Initialize language state
 
-        <Select defaultValue="Hello">
-            <option>Hello</option>
-            <option>Hi</option>
-            <option>Beep</option>
-            <option>Boop</option>
-        </Select>
-    );
-}
+  const changeLanguage = (languageCode) => {
+    setLanguage(languageCode); // Update the language state
+    i18n.changeLanguage(languageCode);
+  };
+
+  return (
+    <Select value={language} onChange={(e) => changeLanguage(e.target.value)}>
+      <option value="es">Es</option>
+      <option value="en">En</option>
+      <option value="ca">Ca</option>
+    </Select>
+  );
+};
 
 export default SelectLang;
